5550834dd2a10410529d6f7afa360df0ce72a536
Tony Santiago <85644672+t0nysantiago@users.noreply.github.com>
2023-12-17 09:45:39 -0300

    Initial Commit

---
+++ ObjectRecognition/ObjectRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectRecognitionApp.swift
+//  ObjectRecognition
+//
+//  Created by Tony Santiago on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ObjectRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
